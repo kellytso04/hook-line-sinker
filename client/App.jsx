d4e34bcb32c409a216b62ada33a2e8255cf572aa
Kelly <kellytso04@gmail.com>
2022-01-20 17:51:36 -0500

    Sets up Webpack-Babel-React

---
+++ client/App.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+
+const samplePhrases = [
+  'Got any WINS for me this morning?',
+  'Let\'s make it a day',
+  'Lemme spin up my announcement machine',
+  'ETL more like Pee-TL',
+  'Postgres took my lunch  money',
+  'I would like to see the baby *vague horn instrument noises*',
+  'Can I please just get a waffle?'
+]
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      message: 'Everything is fine';
+    }
+  }
+
+
+
+  render() {
+
+  }
+
+}
