3314190253cf6e55e3138b03145efc632020abfa
Kelly <kellytso04@gmail.com>
2022-01-20 19:06:48 -0500

    Creates App.jsx with hooks

---
+++ solution/App.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import Message from './Message.jsx';
+import Clock from './Clock.jsx';
+
+/*
+Note 1: If you're not deconstructing your props, please start!
+That way you don't have to write `props.prop` for each one,
+and it greatly improves the readability of your code.
+
+Note 2: You'll notice that this component does not contain a `return` statement;
+that's because if we use parentheses to open the component instead of curly braces,
+you can completely omit the `return` statement.
+Remember that this only works when your component is ONLY returning something.
+It won't work if you have any methods inside!
+*/
+
+const App = ( {samplePhrases} ) => (
+  <div className='app'>
+    <Message samplePhrases={samplePhrases} />
+    <Clock />
+  </div>
+)
+
+export default App;
