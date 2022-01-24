a67b4fb11f5ed92b003fbb68e721b409503df8f6
Kelly <kellytso04@gmail.com>
2022-01-24 17:56:08 -0500

    Adds a TODO

--- client/levelOne/LevelOne.jsx
+++ client/levelOne/LevelOne.jsx
@@ -3,6 +3,7 @@ import Clock from './Clock.jsx';
 import Message from './Message.jsx';
 import RefreshQuoteButton from './RefreshQuoteButton.jsx';
 
+// TODO: Nothing, really. Maybe just a tiny optimization for readability 🤔
 const LevelOne = (props) => (
   <div id='level-one'>
     <h1>Level One: Quote Dropper and a clock</h1>
